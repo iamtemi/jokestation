e7e29dceedcd80677a997cbf73482f6be95b17e8
Temi Adenuga <a.adenuga@hotmail.com>
2024-06-12 10:48:36 +0100

    Update Boilerplate, Create Folder Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import HomePage from "./pages/HomePage/HomePage";
+import SavedJokes from "./pages/SavedJokes/SavedJokes";
+
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
       <h1>JOKE STATION</h1>
-    </>
+      <Header />
+
+      <Routes>
+        <Route path="/start" element={<LandingPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/saved-jokes" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header() {
+  return (
+    <>
+      <Header className="header">
+        <img className="header__img" src="" alt="" />
+        <nav className="header__nav">
+          <ul>
+            <li>Name</li>
+            <li>Home</li>
+            <li>Saved Jokes</li>
+          </ul>
+        </nav>
+      </Header>
+    </>
+  );
+}
