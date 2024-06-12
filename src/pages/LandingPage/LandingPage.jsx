8d797d04bb9513372846e4fe2005cba7f88430c0
Daniel Aston <daniel.aston@live.co.uk>
2024-06-12 14:32:47 +0100

    Create landing page

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,3 +1,43 @@
+import { useState } from "react";
+import "./LandingPage.scss";
+
 export default function LandingPage() {
-  return;
+  //const [category, setCategory] = useState("Any");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.category.value);
+  }
+  return (
+    <section className="landing">
+      <h1 className="landing__title">Joke Station</h1>
+      <p className="landing_instructions">
+        Select a catagory or jump right in!
+      </p>
+      <form className="landing_category" onSubmit={handleSubmit}>
+        {/* Any, Misc, Programming, Dark, Pun, Spooky, Christmas */}
+        <select name="category" className="landing__select" id="category">
+          <option className="landing__option" value="Any">
+            Any
+          </option>
+          <option className="landing__option" value="Misc">
+            Misc
+          </option>
+          <option className="landing__option" value="Programming">
+            Programming
+          </option>
+          <option className="landing__option" value="Dark">
+            Dark
+          </option>
+          <option className="landing__option" value="Spooky">
+            Spooky
+          </option>
+          <option className="landing__option" value="Christmas">
+            Christmas
+          </option>
+        </select>
+        <button className="landing__button">Get Jokes</button>
+      </form>
+    </section>
+  );
 }
