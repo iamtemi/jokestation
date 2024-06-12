1135f77e558e3fd2d49b7f6a0b2f67084721aea5
Daniel Aston <daniel.aston@live.co.uk>
2024-06-12 15:23:49 +0100

    Filtered Jokes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,19 +3,24 @@ import Joke from "../../components/Joke/Joke";
 import axios from "axios";
 import { BeatLoader } from "react-spinners";
 import "./HomePage.scss";
+import { useParams } from "react-router-dom";
 
 export default function HomePage() {
   const [jokes, setJokes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const fetchTenRandomJokes = async () => {
+  const {category} = useParams();
+
+  const fetchTenRandomJokes = async (category, amount) => {
     try {
       const { data } = await axios.get(
-        "https://v2.jokeapi.dev/joke/Any?type=twopart&amount=10"
+        `https://v2.jokeapi.dev/joke/${category}?type=twopart&amount=${amount}`
       );
+
       setJokes(data.jokes);
       setIsLoading(false);
+
     } catch (error) {
       setError(error);
       setIsLoading(false);
@@ -23,8 +28,8 @@ export default function HomePage() {
   };
 
   useEffect(() => {
-    fetchTenRandomJokes();
-  }, []);
+    fetchTenRandomJokes(category, 10);
+  }, [category]);
 
   if (isLoading) {
     return <BeatLoader color="#000000" />;
