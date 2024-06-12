466db1956edb9216d33bd09ff73139f3b93814ff
Temi Adenuga <a.adenuga@hotmail.com>
2024-06-12 12:52:23 +0100

    Add handleClick function to joke article

--- src/components/Joke/Joke.jsx
+++ src/components/Joke/Joke.jsx
@@ -1,10 +1,17 @@
+import { useState } from "react";
 import "./Joke.scss";
 
 export default function Joke({ joke }) {
+  const [jokeSide, setJokeSide] = useState(false);
+
+  const handleClick = () => {
+    setJokeSide(!jokeSide);
+  };
+
   return (
-    <article className="joke">
-      <p className="joke__setup">{joke.setup}</p>
-      <p className="joke__delivery">{joke.delivery}</p>
+    <article className="joke" onClick={handleClick}>
+      {!jokeSide && <p className="joke__setup">{joke.setup}</p>}
+      {jokeSide && <p className="joke__delivery">{joke.delivery}</p>}
       <p className="joke__category">{joke.category}</p>
     </article>
   );
